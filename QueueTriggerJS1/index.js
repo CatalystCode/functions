4791995044eaf93418ab33ab8c1215b00c65d5ec
Sachin Kundu <sachinkundu@gmail.com>
2017-06-21 11:51:58 +0200

    Add predictions to table storage

--- QueueTriggerJS1/index.js
+++ QueueTriggerJS1/index.js
@@ -1,19 +1,40 @@
+var azure = require('azure-storage');
+var fs = require('fs');
+var csv = require('csv');
 module.exports = function (context, jobDescription) {
+    var model_group = jobDescription.model_query.model_group;
+    var model_name = jobDescription.model_query.model_name;
+    var blob_path = model_group + '/' + model_name;
 
-    var Client = require('node-rest-client').Client;
-    var client = new Client();
-    var args = {
-        parameters: { modelurl: {} }
-    };
-    args.parameters['modelurl'] = jobDescription.model_url
-    client.get("http://13.93.204.0:5000/", args, function (data, response) {
-        context.log(response.statusCode);
-        context.log(data.toString());
-        context.bindings.runResult = JSON.stringify({ 
-            id: "12345",
-            prediction: 71.82
-        });
+    var interval = parseInt(jobDescription.model_query.interval);
+    var meantemp = parseFloat(jobDescription.model_query.model_arguments.meantemp);
+    var rainsum = parseFloat(jobDescription.model_query.model_arguments.rainsum);
+    var blobSvc = azure.createBlobService();
+    var outbreak_probability = 0.0;
+    blobSvc.getBlobToText('estrellamlinput', blob_path, function (error, result, response) {
+        if (!error) {
+            csv.parse(result, function (err, output) {
+                for (var index = 1; index < output.length; index++) {
+                    var sample = output[index];
+                    if (parseInt(sample[1]) === interval) {
+                        var eT = Math.exp(parseFloat(sample[2]) + parseFloat(sample[3]) * meantemp + parseFloat(sample[4]) * rainsum);
+                        outbreak_probability = eT / (1 + eT);
+                        context.log('outbreak_prob' + outbreak_probability.toString());
+                        context.bindings.tableBinding = [];
+                        context.bindings.tableBinding.push({
+                            PartitionKey: interval,
+                            RowKey: model_group + '_' + model_name,
+                            Arguments: { rainsum: rainsum, meantemp: meantemp },
+                            Prediction: outbreak_probability
+                        });
+                        context.log("done");
+                        context.done();
+                    }
+                }
+            });
+        } else {
+            context.log(error);
+            context.done();
+        }
     });
-
-    context.done();
 };
