e7873bb869edb34efc59e606914077c2fff06d4d
cloudbeatsch <cloudbeatsch@hotmail.com>
2017-06-24 15:19:33 +0200

    added exception handling to avoid never ending function

--- QueueTriggerJS1/index.js
+++ QueueTriggerJS1/index.js
@@ -2,39 +2,45 @@ var azure = require('azure-storage');
 var fs = require('fs');
 var csv = require('csv');
 module.exports = function (context, jobDescription) {
-    var model_group = jobDescription.model_query.model_group;
-    var model_name = jobDescription.model_query.model_name;
-    var blob_path = model_group + '/' + model_name;
+    try {
+        var model_group = jobDescription.model_query.model_group;
+        var model_name = jobDescription.model_query.model_name;
+        var blob_path = model_group + '/' + model_name;
 
-    var interval = parseInt(jobDescription.model_query.interval);
-    var meantemp = parseFloat(jobDescription.model_query.model_arguments.meantemp);
-    var rainsum = parseFloat(jobDescription.model_query.model_arguments.rainsum);
-    var blobSvc = azure.createBlobService();
-    var outbreak_probability = 0.0;
-    blobSvc.getBlobToText('models', blob_path, function (error, result, response) {
-        if (!error) {
-            csv.parse(result, function (err, output) {
-                for (var index = 1; index < output.length; index++) {
-                    var sample = output[index];
-                    if (parseInt(sample[1]) === interval) {
-                        var eT = Math.exp(parseFloat(sample[2]) + parseFloat(sample[3]) * meantemp + parseFloat(sample[4]) * rainsum);
-                        outbreak_probability = eT / (1 + eT);
-                        context.log('outbreak_prob' + outbreak_probability.toString());
-                        context.bindings.tableBinding = [];
-                        context.bindings.tableBinding.push({
-                            PartitionKey: interval,
-                            RowKey: model_group + '_' + model_name,
-                            Arguments: { rainsum: rainsum, meantemp: meantemp },
-                            Prediction: outbreak_probability
-                        });
-                        context.log("done");
-                        context.done();
+        var interval = parseInt(jobDescription.model_query.interval);
+        var meantemp = parseFloat(jobDescription.model_query.model_arguments.meantemp);
+        var rainsum = parseFloat(jobDescription.model_query.model_arguments.rainsum);
+        var blobSvc = azure.createBlobService();
+        var outbreak_probability = 0.0;
+        blobSvc.getBlobToText('models', blob_path, function (error, result, response) {
+            if (!error) {
+                csv.parse(result, function (err, output) {
+                    for (var index = 1; index < output.length; index++) {
+                        var sample = output[index];
+                        if (parseInt(sample[1]) === interval) {
+                            var eT = Math.exp(parseFloat(sample[2]) + parseFloat(sample[3]) * meantemp + parseFloat(sample[4]) * rainsum);
+                            outbreak_probability = eT / (1 + eT);
+                            context.log('outbreak_prob' + outbreak_probability.toString());
+                            context.bindings.tableBinding = [];
+                            context.bindings.tableBinding.push({
+                                PartitionKey: interval,
+                                RowKey: model_group + '_' + model_name,
+                                Arguments: { rainsum: rainsum, meantemp: meantemp },
+                                Prediction: outbreak_probability
+                            });
+                            context.log("done");
+                            context.done();
+                        }
                     }
-                }
-            });
-        } else {
-            context.log.error(error);
-            context.done(error);
-        }
-    });
+                });
+            } else {
+                context.log.error(error);
+                context.done(error);
+            }
+        });
+    }
+    catch (ex) {
+        context.log.error(ex.message);
+        context.done(ex);       
+    }
 };
