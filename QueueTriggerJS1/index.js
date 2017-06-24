187c57b3369826bff647df2fb9759ed0621117cc
cloudbeatsch <cloudbeatsch@hotmail.com>
2017-06-24 16:01:44 +0200

    added instrumentation for debugging function in Azure

--- QueueTriggerJS1/index.js
+++ QueueTriggerJS1/index.js
@@ -6,31 +6,41 @@ module.exports = function (context, jobDescription) {
         var model_group = jobDescription.model_query.model_group;
         var model_name = jobDescription.model_query.model_name;
         var blob_path = model_group + '/' + model_name;
+        context.log("target model: " + blob_path);
 
         var interval = parseInt(jobDescription.model_query.interval);
         var meantemp = parseFloat(jobDescription.model_query.model_arguments.meantemp);
         var rainsum = parseFloat(jobDescription.model_query.model_arguments.rainsum);
+        context.log("arguments: " + interval + ", " + meantemp + ", " + rainsum);    
+
         var blobSvc = azure.createBlobService();
         var outbreak_probability = 0.0;
         blobSvc.getBlobToText('models', blob_path, function (error, result, response) {
             if (!error) {
+                context.log("parse csv file");
                 csv.parse(result, function (err, output) {
-                    for (var index = 1; index < output.length; index++) {
-                        var sample = output[index];
-                        if (parseInt(sample[1]) === interval) {
-                            var eT = Math.exp(parseFloat(sample[2]) + parseFloat(sample[3]) * meantemp + parseFloat(sample[4]) * rainsum);
-                            outbreak_probability = eT / (1 + eT);
-                            context.log('outbreak_prob' + outbreak_probability.toString());
-                            context.bindings.tableBinding = [];
-                            context.bindings.tableBinding.push({
-                                PartitionKey: interval,
-                                RowKey: model_group + '_' + model_name,
-                                Arguments: { rainsum: rainsum, meantemp: meantemp },
-                                Prediction: outbreak_probability
-                            });
-                            context.log("done");
-                            context.done();
+                    if (!err) {
+                        for (var index = 1; index < output.length; index++) {
+                            var sample = output[index];
+                            if (parseInt(sample[1]) === interval) {
+                                var eT = Math.exp(parseFloat(sample[2]) + parseFloat(sample[3]) * meantemp + parseFloat(sample[4]) * rainsum);
+                                outbreak_probability = eT / (1 + eT);
+                                context.log('outbreak_prob' + outbreak_probability.toString());
+                                context.bindings.tableBinding = [];
+                                context.bindings.tableBinding.push({
+                                    PartitionKey: interval,
+                                    RowKey: model_group + '_' + model_name,
+                                    Arguments: { rainsum: rainsum, meantemp: meantemp },
+                                    Prediction: outbreak_probability
+                                });
+                                context.log("done");
+                                context.done();
+                            }
                         }
+                    }
+                    else {
+                        context.log.error(err);
+                        context.done(err);
                     }
                 });
             } else {
@@ -41,6 +51,6 @@ module.exports = function (context, jobDescription) {
     }
     catch (ex) {
         context.log.error(ex.message);
-        context.done(ex);       
+        context.done(ex.message);       
     }
 };
